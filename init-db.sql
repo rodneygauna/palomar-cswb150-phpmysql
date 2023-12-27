3e1c11345f4bdb14d49a5e0a22ffe30fc094bff9
Rodney Gauna <rodneygauna@gmail.com>
2023-12-27 12:00:35 -0800

    adding init-db to the project

---
+++ init-db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS mydatabase;
+USE mydatabase;
+
+CREATE TABLE IF NOT EXISTS example_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    data VARCHAR(255) NOT NULL
+);
+
+-- Add more table creation queries or initial data inserts as needed.
