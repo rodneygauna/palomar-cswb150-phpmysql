558a14d967bf5f57de79ae55b6a80032d132e153
Rodney Gauna <rodneygauna@gmail.com>
2024-01-29 10:04:48 -0800

    removed intro html

--- www/10-phpIntroduction/X_3_2.php
+++ www/10-phpIntroduction/X_3_2.php
@@ -1,32 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+print "<html>\n";
+print "<head><title>0302 Displaying HTML</title>\n";
+print "<style>body {font-family: Arial, Helvetica, sans-serif; font-size: 16px;}</style>\n";
+print "</head>";
+print "\n ";
+print "<body>\n";
+print "	<h1>0302 Displaying HTML</h1>\n";
+print "	<p>What are script tags and why do we need them to run PHP?</p>\n";
+print "\n ";
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercise 3-2</title>
-</head>
+$today = date('Y-m-d h:i:s');
 
-<body>
-    <?php
-    print "<html>\n";
-    print "<head><title>0302 Displaying HTML</title>\n";
-    print "<style>body {font-family: Arial, Helvetica, sans-serif; font-size: 16px;}</style>\n";
-    print "</head>";
-    print "\n ";
-    print "<body>\n";
-    print "	<h1>0302 Displaying HTML</h1>\n";
-    print "	<p>What are script tags and why do we need them to run PHP?</p>\n";
-    print "\n ";
-
-    $today = date('Y-m-d h:i:s');
-
-    print "<p><b>Today is $today </b></p>\n";
-    print " ";
-    print "</body>\n";
-    print " ";
-    print "</html>\n";
-    ?>
-</body>
-
-</html>
+print "<p><b>Today is $today </b></p>\n";
+print " ";
+print "</body>\n";
+print " ";
+print "</html>\n";
